0e5d0bc12431e7aa14b889f2ad7b614b2a3d9733
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-06 14:25:30 +0900

    初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AchsFes2015::Application.config.secret_key_base = '477f00fd8109048731a8c8a7fcd8440c71bb1178c3a1a885436b4f0d28ab12fc7e2016936f4869d86bd08d7c1350b1ed533b025734bc91c108f22f2555e626b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AchsFes2015::Application.config.session_store :cookie_store, key: '_achs_fes_2015_session'
