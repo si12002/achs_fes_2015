20bb58cae4d55cf20fe3ccfa24fed0fd525a1d84
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-09 22:05:49 +0900

    リンクの追加とテスト

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,32 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'AchsFes2015'" do
-      visit '/static_pages/home'
+    it "should have the h1 '青商祭2015'" do
+      visit root_path
       expect(page).to have_content('青商祭2015')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("青商祭2015")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+    it "should have the h1 'このアプリについて'" do
+      visit about_path
       expect(page).to have_content('このアプリについて')
     end
 
-    it "should not have a custom page title" do
-      visit '/static_pages/about'
-      expect(page).not_to have_title("| About Us")
+    it "should have the title 'このアプリについて'" do
+      visit about_path
+      expect(page).to have_title("青商祭2015 | このアプリについて")
     end
 
   end
