ed8c6696bd601aa52509d7767ec1d1d865b9d1f1
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-13 13:41:29 +0900

    ユーザーの検証を追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812143609) do
+ActiveRecord::Schema.define(version: 20150813043817) do
 
   create_table "homerooms", force: true do |t|
     t.string   "shop_name"
