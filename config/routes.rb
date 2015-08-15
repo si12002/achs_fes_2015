9bec16be2bae78a8cb876a3e17e7054d21c416a8
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-15 16:09:33 +0900

    ユーザーの情報を表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 AchsFes2015::Application.routes.draw do
+  resources :users
   root 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/disp_3rd', to: 'static_pages#disp_3rd', via: 'get'
