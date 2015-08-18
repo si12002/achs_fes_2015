e8d44c2cad8da28d441cf3d33f8dc14a95581172
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-18 10:38:43 +0900

    サインイン・サインアウトができるようにする

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -2,12 +2,42 @@ require 'spec_helper'
 
 describe "Authentication" do
 
-  subject { page }
+	subject { page }
 
-  describe "signin page" do
-    before { visit signin_path }
+  	describe "signin page" do
+    	before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
-  end
+    	it { should have_content('ログイン') }
+    	it { should have_title('ログイン') }
+  	end
+
+  	describe "signin" do
+    	before { visit signin_path }
+
+    	describe "with invalid information" do
+      		before { click_button "ログイン" }
+
+      		it { should have_title('ログイン') }
+      		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    	end
+
+    	describe "with valid information" do
+      		let(:user) { FactoryGirl.create(:user) }
+      		before do
+        		fill_in "メールアドレス", with: user.email.upcase
+        		fill_in "パスワード", with: user.password
+        		click_button "ログイン"
+      		end
+
+      		it { should have_title(user.name) }
+      		# it { should have_link('Profile', href: user_path(user)) }
+      		it { should have_link('ログアウト', href: signout_path) }
+      		it { should_not have_link('Sign in', href: signin_path) }
+    	
+    		describe "followed by signout" do
+        		before { click_link "ログアウト" }
+        		it { should have_link('ログイン') }
+      		end
+    	end
+  	end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 describe "User pages" do
 
-  subject { page }
+  	subject { page }
 
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+	describe "profile page" do
+    	let(:user) { FactoryGirl.create(:user) }
+    	before { visit user_path(user) }
 
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
-  end
+    	it { should have_content(user.name) }
+    	it { should have_title(user.name) }
+  	end
+
+  	describe "with valid information" do
+    	describe "after saving the user" do
+    		before { click_link "ログイン" }
+        	let(:user) { User.find_by(email: 'user@example.com') }
+
+        	it { should have_link('ログアウト') }
+        	it { should have_title(user.name) }
+        	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    	end
+    end
 end
