89a07fd6f7cfbf91adb1691658a17ae37fefab4b
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-21 18:13:03 +0900

    アクセス制御をおこなうようにする

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -63,6 +63,19 @@ describe "Authentication" do
             specify { expect(response).to redirect_to(signin_path) }
           end
         end
+
+        describe "in the Posts controller" do
+
+        describe "submitting to the create action" do
+          before { post posts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete post_path(FactoryGirl.create(:post)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
       end
     end
 end
