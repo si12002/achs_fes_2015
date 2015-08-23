c67c146f09152b9150434c59e0f5c248cc462a8a
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-23 14:26:34 +0900

    Homeroom（プロフィール）の編集をできるようにする

--- app/controllers/homerooms_controller.rb
+++ app/controllers/homerooms_controller.rb
@@ -1,5 +1,5 @@
 class HomeroomsController < ApplicationController
-before_action :signed_in_user, only: [:create, :destroy]
+before_action :signed_in_user, only: [:create, :edit, :update, :destroy]
 
 def index
 	@homerooms = Homeroom.all
@@ -7,7 +7,27 @@ end
 
 def show
 	@homeroom = Homeroom.find(params[:id])
+	@user = User.find(params[:id])
 	@posts = @homeroom.posts
 end
+
+def edit
+	@homeroom = Homeroom.find(params[:id])
+end
+
+def update
+	@homeroom = Homeroom.find(params[:id])
+	if @homeroom.update_attributes(homeroom_params)
+      flash[:success] = "変更を保存しました。"
+      redirect_to @homeroom
+    else
+      render 'edit'
+    end
+end
+
+private
+    def homeroom_params
+      params.require(:homeroom).permit(:shop_name, :content, :menu)
+    end
 
 end
