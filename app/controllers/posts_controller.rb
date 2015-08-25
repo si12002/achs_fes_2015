0b2b7784cff64cb9ef0b58f69ea69bf35e7e917e
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-25 13:39:04 +0900

    記事を変更する機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,20 @@ class PostsController < ApplicationController
     	end
   	end
 
+  	def edit
+  		@post = Post.find(params[:id])
+  	end
+
+  	def update
+  		@post = Post.find(params[:id])
+  		if @post.update_attributes(post_params)
+      		flash[:success] = "変更を保存しました。"
+      		redirect_to @post
+    	else
+      		render 'edit'
+    	end
+  	end
+
 	def destroy
 		@post.destroy
     	redirect_to current_user
