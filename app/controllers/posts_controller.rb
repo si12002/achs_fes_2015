4f6fb72096c2413ab54b9a857c718bcceec19225
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-25 11:40:35 +0900

    削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	before_action :signed_in_user, only: [:new, :create, :destroy]
+	before_action :correct_user,   only: :destroy
 
 	def index
 		@posts = Post.all
@@ -28,12 +29,21 @@ class PostsController < ApplicationController
   	end
 
 	def destroy
+		@post.destroy
+    	redirect_to current_user
 	end
 
 
 	private
     	def post_params
       		params.require(:post).permit(:title, :content)
+    	end
+
+    	def correct_user
+      		# @post = current_user.posts.find_by(id: params[:id])
+      		@homeroom = Homeroom.find(current_user.id)
+      		@post = @homeroom.posts.find_by(id: params[:id])
+      		redirect_to current_user if @post.nil?
     	end
 
 end
