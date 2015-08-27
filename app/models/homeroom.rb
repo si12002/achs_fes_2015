7453d04c532bf6b84c08ba97856bcd310426563b
石川佳実 <si12002@edu.aomori-u.ac.jp>
2015-08-27 14:21:40 +0900

    検証の追加

--- app/models/homeroom.rb
+++ app/models/homeroom.rb
@@ -1,4 +1,6 @@
 class Homeroom < ActiveRecord::Base
 	has_one :users, dependent: :destroy
 	has_many :posts, dependent: :destroy
+	validates :shop_name, presence: true
+	validates :content, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :homeroom
 	default_scope -> { order('created_at DESC') }
+	validates :title, presence: true
+	validates :content, presence: true
   	validates :homeroom_id, presence: true
 end
